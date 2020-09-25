cc0829e4bde9b77e6a41645cf34dc64c95deab36
Shashaank <shankisri@gmail.com>
2020-09-25 11:02:28 -0400

    Improved the methods

--- BuddyInfo/src/AddressBook.java
+++ BuddyInfo/src/AddressBook.java
@@ -5,21 +5,29 @@ public class AddressBook {
     private List<BuddyInfo> buddies;
     public AddressBook(){
         buddies = new ArrayList();
-        buddies.add(new BuddyInfo("Myself", "613", "Carleton"));
-        buddies.add(new BuddyInfo("bobla", "613 737", "OttawaU"));
-        buddies.add(new BuddyInfo("pogger", "613 737 1111", "the gonq"));
-        buddies.add(new BuddyInfo("chicken eater", "613 1234", "N/A"));
+
     }
 
     public static void main(String[] args) {
         System.out.println("AddressBook");
+        AddressBook first = new AddressBook();
+        first.addBuddy(new BuddyInfo("Myself", "613", "Carleton"));
+
+        first.addBuddy(new BuddyInfo("bobla", "613 737", "OttawaU"));
+        /*
+        buddies.add(new BuddyInfo("pogger", "613 737 1111", "the gonq"));
+        buddies.add(new BuddyInfo("chicken eater", "613 1234", "N/A"));
+
+         */
+        first.removeBuddy(new BuddyInfo("bobla", "613 737", "OttawaU"));
+
         // adding a random comment to test git
     }
 
-    private void addBuddy(String name){
-        return;
+    private void addBuddy(BuddyInfo buddy){
+        buddies.add(buddy);
     }
-    private void removeBuddy(String name){
-        return;
+    private void removeBuddy(BuddyInfo buddy){
+        buddies.remove(buddy);
     }
 }
