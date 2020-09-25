f7a3e1cc92f0806743aaa5d884324e9e2e23cfb0
Shashaank <shankisri@gmail.com>
2020-09-25 10:43:59 -0400

    added main method

---
+++ BuddyInfo/src/AddressBook.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<BuddyInfo> buddies;
+    public AddressBook(){
+        buddies = new ArrayList();
+        buddies.add(new BuddyInfo("Myself", "613", "Carleton"));
+        buddies.add(new BuddyInfo("bobla", "613 737", "OttawaU"));
+        buddies.add(new BuddyInfo("pogger", "613 737 1111", "the gonq"));
+        buddies.add(new BuddyInfo("chicken eater", "613 1234", "N/A"));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("AddressBook");
+    }
+
+    private void addBuddy(String name){
+        return;
+    }
+    private void removeBuddy(String name){
+        return;
+    }
+}
