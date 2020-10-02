c913018fb1862e257b3ed6983e3389863e4712f0
Shashaank <shankisri@gmail.com>
2020-10-02 09:24:33 -0400

    added github support

---
+++ BuddyInfo/src/BuddyInfo.java
@@ -0,0 +1,38 @@
+
+public class BuddyInfo {
+	
+	private String name;
+	private String phone;
+	private String school;
+	
+	public BuddyInfo(String name, String phone, String school) {
+		this.name = name;
+		this.phone = phone;
+		this.school = school;
+	}
+	
+	
+	
+	 public static void main(String[] args) {
+		 BuddyInfo bob = new BuddyInfo("Homer", "613-112-1123", "Ottawa University");
+		 System.out.println("Hello Universe"); 
+	 }
+
+
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+	 
+	 
+	 
+
+}
